ac4846763ee5985b72f7fb2072ce43cdf7b43317
Filip-Bumbu <filipb@telnyx.com>
2025-02-24 16:44:02 +0000

    Setup X2

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 
 class Main {
-    
+    //> 1. Farm.java
+    //! 2. Field.java
+    //! 3. Farmer.java
+    //! 4. Buyer.java
+    //! 5. DeliveryManager.java
+    //! 6. TickManager.java
+    //! 7. Main.java
 }
