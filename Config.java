0f46164740a6c6e008555b0a4a058197e10549b4
cdobey <chris.dobey@r3.com>
2025-03-10 15:49:44 +0000

    Update Config.java

--- Config.java
+++ Config.java
@@ -1,7 +1,7 @@
 public class Config {
     public static int TICK_SIZE = 100;
-    public static int NUMBER_OF_FARMERS = 7;
+    public static int NUMBER_OF_FARMERS = 3;
     public static int NUMBER_OF_BUYERS = 3;
-    public static int DELIVERY_FREQUENCY = 10;
+    public static int DELIVERY_FREQUENCY = 100;
     public static int SIMULATION_DURATION = 10000;
 }
